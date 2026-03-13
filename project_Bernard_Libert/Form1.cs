cc52eb6e9a1b173126f9f7cc35f7bf8606af71e1
Thomas Lerouge <thomas.lerouge@leerling.go-ao.be>
2026-03-13 21:33:04 +0100

    UI make over gegeven

--- project_Bernard_Libert/Form1.cs
+++ project_Bernard_Libert/Form1.cs
@@ -20,230 +20,160 @@ namespace project_Bernard_Libert
         ComboBox comboBox = new ComboBox();
 
         Panel panelHome = new Panel();
+        Panel PanelRijen = new Panel();
+
+        int aantalProjecten = 0;
+
+        List<Project> alleProjecten = new List<Project>();
 
         public Form1()
         {
             InitializeComponent();
-            this.WindowState = FormWindowState.Maximized;
+            this.Size = new Size(1200, 800);
 
             loadMain();
             loadMenuPanel();
             loadHeaderPanel();
+
+            loadProjecten();
         }
         void loadHeaderPanel()
         {
             Panel headerPanel = new Panel();
-            headerPanel.Dock = DockStyle.Top;
-            headerPanel.Height = 80;
+            headerPanel.Location = new Point(0, 0);
+            headerPanel.Size = new Size(1200, 80);
             headerPanel.BackColor = Color.FromArgb(24, 28, 38);
+            headerPanel.BorderStyle = BorderStyle.FixedSingle;
+
             Controls.Add(headerPanel);
+
+            Button buttonAddProject = new Button();
+            buttonAddProject.Text = "+ Nieuw Project";
+            buttonAddProject.Location = new Point(1000, 15);
+            buttonAddProject.Size = new Size(150, 50);
+            buttonAddProject.BackColor = Color.FromArgb(0, 122, 204);
+            buttonAddProject.FlatStyle = FlatStyle.Flat;
+            buttonAddProject.FlatAppearance.BorderSize = 0;
+            buttonAddProject.ForeColor = Color.White;
+            buttonAddProject.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+            headerPanel.Controls.Add(buttonAddProject);
+
+            Label lblTitel = new Label();
+            lblTitel.Text = "Projecten\r\nBernard Libert";
+            lblTitel.Location = new Point(20, 10);
+            lblTitel.ForeColor = Color.White;
+            lblTitel.Font = new Font("Segoe UI", 16, FontStyle.Bold);
+            lblTitel.AutoSize = true;
+            headerPanel.Controls.Add(lblTitel);
+
         }
         void loadMenuPanel()
         {
             Panel menuPanel = new Panel();
-            menuPanel.Dock = DockStyle.Left;
-            menuPanel.Width = 200;
-            menuPanel.BackColor = Color.Gray;
+            menuPanel.Location = new Point(0, 80);
+            menuPanel.Size = new Size(200, 720);
+            menuPanel.BorderStyle = BorderStyle.FixedSingle;
+            menuPanel.BackColor = Color.FromArgb(21, 25, 50);
             Controls.Add(menuPanel);
 
-            Label lblHome = new Label();
-            lblHome.BackColor = Color.DarkGray;
-            lblHome.Location = new Point(0, 0);
-            lblHome.Size = new Size(menuPanel.Width, 50);
-            lblHome.ForeColor = Color.White;
-            lblHome.Text = "home";
-            lblHome.TextAlign = ContentAlignment.MiddleCenter;
-            lblHome.Font = new Font(lblHome.Font.FontFamily,15,FontStyle.Bold);
-            menuPanel.Controls.Add(lblHome);
+            Label lblAllProjects = new Label();
+            lblAllProjects.BackColor = Color.FromArgb(21, 25, 50);
+            lblAllProjects.Location = new Point(0, 0);
+            lblAllProjects.Size = new Size(menuPanel.Width, 50);
+            lblAllProjects.ForeColor = Color.White;
+            lblAllProjects.Text = "Alle Projecten";
+            lblAllProjects.BorderStyle = BorderStyle.FixedSingle;
+            lblAllProjects.TextAlign = ContentAlignment.MiddleCenter;
+            lblAllProjects.Font = new Font(lblAllProjects.Font.FontFamily, 15, FontStyle.Regular);
+            menuPanel.Controls.Add(lblAllProjects);
         }
         void loadMain()
         {
-            panelHome.Dock = DockStyle.Fill;
+            panelHome.Location = new Point(200, 80);
+            panelHome.Size = new Size(1000, 720);
+            panelHome.ForeColor = Color.White;
+            panelHome.BackColor = Color.FromArgb(18, 23, 29);
+            panelHome.BorderStyle = BorderStyle.FixedSingle;
             Controls.Add(panelHome);
 
-            Button buttonNewProject = new Button();
-            buttonNewProject.Text = "projectje toevoegen";
-            buttonNewProject.Location = new Point(0, 0);
-            buttonNewProject.Size = new Size(300, 100);
-            buttonNewProject.Font = new Font("Segoe UI", 18, FontStyle.Bold);
-            panelHome.Controls.Add(buttonNewProject);
+            Panel panelKoprij = new Panel();
+            panelKoprij.Location = new Point(20, 10);
+            panelKoprij.Size = new Size(600, 40);
+            panelKoprij.BackColor = Color.FromArgb(34, 38, 48);
+            panelKoprij.ForeColor = Color.White;
+            panelHome.Controls.Add(panelKoprij);
 
-            buttonNewProject.Click += (sender, e) =>
-            {
-                FormAdd formAdd = new FormAdd();
-                formAdd.Show();
-                formAdd.Ready += (sender2, e2) =>
-                {
-                    addRow(formAdd);
-                    formAdd.Close();
-                };
-            };
+            // naam, klant, aannemer
+            Label koprijNaam = new Label();
+            koprijNaam.Text = "Naam";
+            koprijNaam.Location = new Point(0, 5);
+            koprijNaam.Size = new Size(200, 30);
+            koprijNaam.TextAlign = ContentAlignment.MiddleCenter;
+            koprijNaam.Font = new Font(koprijNaam.Font.FontFamily, 12, FontStyle.Regular);
+            panelKoprij.Controls.Add(koprijNaam);
 
-            Button buttonToonProjecten = new Button();
-            buttonToonProjecten.Text = "Toon projecten";
-            buttonToonProjecten.Location = new Point(350, 100);
-            buttonToonProjecten.Size = new Size(300, 100);
-            buttonToonProjecten.Font = new Font("Segoe UI", 18, FontStyle.Bold);
-            panelHome.Controls.Add(buttonToonProjecten);
+            Label koprijKlant = new Label();
+            koprijKlant.Text = "Klant";
+            koprijKlant.Location = new Point(200, 5);
+            koprijKlant.Size = new Size(200, 30);
+            koprijKlant.TextAlign = ContentAlignment.MiddleCenter;
+            koprijKlant.Font = new Font(koprijNaam.Font.FontFamily, 12, FontStyle.Regular);
+            panelKoprij.Controls.Add(koprijKlant);
 
-            buttonToonProjecten.Click += (sender, e) =>
-            {
-                loadProjecten();
-            };
+            Label koprijAannemer = new Label();
+            koprijAannemer.Text = "Aannemer";
+            koprijAannemer.Location = new Point(400, 5);
+            koprijAannemer.Size = new Size(200, 30);
+            koprijAannemer.TextAlign = ContentAlignment.MiddleCenter;
+            koprijAannemer.Font = new Font(koprijNaam.Font.FontFamily, 12, FontStyle.Regular);
+            panelKoprij.Controls.Add(koprijAannemer);
+
+
+            PanelRijen.Location = new Point(10, 60);
+            PanelRijen.Size = new Size(620, 600);
+            PanelRijen.BackColor = Color.FromArgb(18, 23, 29);
+            PanelRijen.BorderStyle = BorderStyle.FixedSingle;
+            PanelRijen.ForeColor = Color.White;
+            PanelRijen.AutoScroll = true;
+            panelHome.Controls.Add(PanelRijen);
+
         }
         void loadProjecten()
         {
+            projectenInlezen();
 
-            comboBox.Location = new Point(10, 60);
-            Controls.Add(comboBox);
-
-            dataGridView.Location = new Point(10, 100);
-            dataGridView.Size = new Size(500, 300);
-            dataGridView.ScrollBars = ScrollBars.Vertical;
-            panelHome.Controls.Add(dataGridView);
-
-            Button buttonNewProject = new Button();
-            buttonNewProject.Text = "project toevoegen";
-            buttonNewProject.Location = new Point(10, 410);
-            buttonNewProject.Size = new Size(100, 50);
-            panelHome.Controls.Add(buttonNewProject);
-
-            buttonNewProject.Click += (sender, e) =>
+            for (int i = 0; i < alleProjecten.Count; i++)
             {
-                FormAdd formAdd = new FormAdd();
-                formAdd.Show();
-                formAdd.Ready += (sender2, e2) =>
-                {
-                    addRow(formAdd);
-                    formAdd.Close();
-                };
-            };
+                Panel panelRij = new Panel();
+                panelRij.Location = new Point(10, 10 + (i * 40) + (i * 5));
+                panelRij.Size = new Size(600, 40);
+                panelRij.BackColor = Color.FromArgb(40, 40, 50);
+                panelRij.BorderStyle = BorderStyle.FixedSingle;
+                PanelRijen.Controls.Add(panelRij);
 
-            Button buttonToonAlles = new Button();
-            buttonToonAlles.Text = "toon alle projecten";
-            buttonToonAlles.Location = new Point(110, 410);
-            buttonToonAlles.Size = new Size(100, 50);
-            panelHome.Controls.Add(buttonToonAlles);
+                Label rijNaam = new Label();
+                rijNaam.Text = alleProjecten[i].Naam;
+                rijNaam.Location = new Point(0, 5);
+                rijNaam.Size = new Size(200, 30);
+                rijNaam.TextAlign = ContentAlignment.MiddleCenter;
+                rijNaam.Font = new Font(rijNaam.Font.FontFamily, 12, FontStyle.Regular);
+                panelRij.Controls.Add(rijNaam);
 
-            buttonToonAlles.Click += (sender, e) =>
-            {
-                comboBox.Text = string.Empty;
-                showAllProjects();
-            };
+                Label rijKlant = new Label();
+                rijKlant.Text = alleProjecten[i].Klant;
+                rijKlant.Location = new Point(200, 5);
+                rijKlant.Size = new Size(200, 30);
+                rijKlant.TextAlign = ContentAlignment.MiddleCenter;
+                rijKlant.Font = new Font(rijKlant.Font.FontFamily, 12, FontStyle.Regular);
+                panelRij.Controls.Add(rijKlant);
 
-            Label lblEditInfo = new Label();
-            lblEditInfo.Text = "Klik op de id van een project\r\nom de info ervan te bewerken";
-            lblEditInfo.Location = new Point(550, 100);
-            lblEditInfo.ForeColor = Color.Red;
-            lblEditInfo.BorderStyle = BorderStyle.FixedSingle;
-            lblEditInfo.AutoSize = true;
-            panelHome.Controls.Add(lblEditInfo);
-
-            Label lblDeleteInfo = new Label();
-            lblDeleteInfo.Text = "Klik op de lege plaats voor een project\r\nom het project te verwijderen";
-            lblDeleteInfo.Location = new Point(550, 150);
-            lblDeleteInfo.ForeColor = Color.Red;
-            lblDeleteInfo.BorderStyle = BorderStyle.FixedSingle;
-            lblDeleteInfo.AutoSize = true;
-            panelHome.Controls.Add(lblDeleteInfo);
-
-            dataGridView.CellClick += (sender, e) =>
-            {
-                // e is van het type DataGridViewCellEventArgs
-                int rij = e.RowIndex;
-                int kolom = e.ColumnIndex;
-                Console.WriteLine($"Je hebt geklikt op rij {rij} en kolom {kolom}");
-
-                if (kolom == 0)
-                {
-                    string waarde = dataGridView.Rows[rij].Cells[kolom].Value.ToString();
-                    FormEdit formEdit = new FormEdit();
-                    formEdit.Show();
-
-                    formEdit.EditReady += (sender2, e2) =>
-                    {
-                        editRow(formEdit, waarde);
-                        formEdit.Close();
-                    };
-                }
-                if (kolom == -1)
-                {
-                    string werf = dataGridView.Rows[rij].Cells[kolom + 1].Value.ToString();
-                    deleteRow(werf);
-                }
-            };
-
-            showAllProjects();
-
-            loadComboBox(comboBox);
-
-            comboBox.SelectedIndexChanged += (sender, e) =>
-            {
-                string werf = comboBox.SelectedItem.ToString();
-                showProject(werf);
-            };
-        }
-        void loadComboBox(ComboBox comboBox)
-        {
-            comboBox.Items.Clear();
-            string query = "SELECT werf FROM Bernard_Libert";
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                // Open the connection
-                connection.Open();
-                Console.WriteLine("Connection successfully established!");
-                using (MySqlCommand command = new MySqlCommand(query, connection))
-                {
-                    using (MySqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            string werf = reader.GetString("werf");
-                            comboBox.Items.Add(werf);
-                        }
-                    }
-                }
-
-            }
-        }
-        void showProject(string werf)
-        {
-            string query = $"select * from Bernard_Libert where werf = '{werf}'";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                // Open the connection
-                connection.Open();
-                Console.WriteLine("Connection successfully established!");
-
-                // maak een data adapter (dit maakt ahw een verbinding ts de server en DataTable
-                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
-                // maak een DataTable: dit object kan tabel gegevens bijhouden
-                DataTable table = new DataTable();
-                // vraag aan de data adaptor om de tabel op te vullen
-                adapter.Fill(table);
-                // gebruik tenslotte de tabel als gegevensbron voor de DataGridView
-                dataGridView.DataSource = table;
-            }
-        }
-        void showAllProjects()
-        {
-            string query = $"select * from Bernard_Libert";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                // Open the connection
-                connection.Open();
-                Console.WriteLine("Connection successfully established!");
-
-                // maak een data adapter (dit maakt ahw een verbinding ts de server en DataTable
-                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
-                // maak een DataTable: dit object kan tabel gegevens bijhouden
-                DataTable table = new DataTable();
-                // vraag aan de data adaptor om de tabel op te vullen
-                adapter.Fill(table);
-                // gebruik tenslotte de tabel als gegevensbron voor de DataGridView
-                dataGridView.DataSource = table;
+                Label rijAannemer = new Label();
+                rijAannemer.Text = alleProjecten[i].Aannemer;
+                rijAannemer.Location = new Point(400, 5);
+                rijAannemer.Size = new Size(200, 30);
+                rijAannemer.TextAlign = ContentAlignment.MiddleCenter;
+                rijAannemer.Font = new Font(rijAannemer.Font.FontFamily, 12, FontStyle.Regular);
+                panelRij.Controls.Add(rijAannemer);
             }
         }
         void addRow(FormAdd formAdd)
@@ -260,8 +190,6 @@ namespace project_Bernard_Libert
                     command.Parameters.AddWithValue("@extra_info", formAdd.addExtraInfo);
                     int rowsAffected = command.ExecuteNonQuery(); // Voert de INSERT uit
                     Console.WriteLine($"{rowsAffected} rij(en) toegevoegd.");
-                    showAllProjects();
-                    loadComboBox(comboBox);
                 }
 
             }
@@ -280,8 +208,6 @@ namespace project_Bernard_Libert
                     command.Parameters.AddWithValue("@extra_info", formEdit.editExtraInfo);
                     int rowsAffected = command.ExecuteNonQuery(); // Voert de INSERT uit
                     Console.WriteLine($"{rowsAffected} rij(en) toegevoegd.");
-                    showAllProjects();
-                    loadComboBox(comboBox);
                 }
 
             }
@@ -297,10 +223,34 @@ namespace project_Bernard_Libert
                     command.Parameters.AddWithValue("@werf", werf);
                     int rowsAffected = command.ExecuteNonQuery(); // Voert de INSERT uit
                     Console.WriteLine($"{rowsAffected} rij(en) toegevoegd.");
-                    showAllProjects();
-                    loadComboBox(comboBox);
                 }
 
+            }
+        }
+        void projectenInlezen()
+        {
+            string query = "SELECT * FROM Bernard_Libert";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                // Open the connection
+                connection.Open();
+                Console.WriteLine("Connection successfully established!");
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Project project = new Project();
+                            project.Naam = reader.GetString("werf");
+                            project.Klant = reader.GetString("klant");
+                            project.Aannemer = reader.GetString("aannemer");
+                            project.ExtraInfo = reader.GetString("extra_info");
+                            alleProjecten.Add(project);
+                        }
+                    }
+                }
             }
         }
     }
